2ffe5c5a48a176e9d6730dbd566b8380adb9527d
Marco <1583606+VaultDeveloper@users.noreply.github.com>
2021-08-21 15:30:38 +0200

    Refacto + commentaires.

--- france-culture-downloader.js
+++ france-culture-downloader.js
@@ -4,26 +4,36 @@ document.body.style.border = "5px solid orange";
 
 console.log(logId, "Plugin loaded.");
 
+/** Retourne la balise HTML `<button>` à ajouter au `DOM`.
+ * @returns Balise HTML `<button>` à ajouter au `DOM`.
+*/
+function buildDownloadButton() {
+		/** Balise HTML `button` pour télécharger le podcast. */
+		const downloadButton = document.createElement("button");
+		// Ajoute la CSS et le texte.
+		downloadButton.className =
+			"replay-button paused aod playable blue textualized playing";
+		downloadButton.style = "font-size: inherit;";
+		downloadButton.innerHTML = "Télécharger";
+
+		downloadButton.addEventListener("click", () => {
+			console.log(logId, "Download button clicked.");
+			downloadRequestPort.postMessage({ url: getFileUrl(domButtons) });
+		});
+}
+
+/** Ajoute le bouton de téléchargement sur la page si elle contient un podcast téléchargeable. */
 function addDownloadButton() {
+	/** Div qui doit contenir le bouton `Écouter` et sur lequel il faut ajouter le bouton `Télécharger`. */
 	const domButtons = document.querySelector("div.buttons");
 
 	if (domButtons) {
 		console.log(logId, "buttons detected");
 		document.body.style.border = "5px solid green";
 
-		const downloadButton = document.createElement("button");
-		downloadButton.className =
-			"replay-button paused aod playable blue textualized playing";
-		downloadButton.style = "font-size: inherit;";
-		downloadButton.innerHTML = "Télécharger";
-		domButtons.appendChild(downloadButton);
-
-		downloadButton.addEventListener("click", () => {
-			console.log(logId, "Download button clicked.");
-			downloadRequestPort.postMessage({ url: getFileUrl(domButtons) });
-		});
+		domButtons.appendChild(buildDownloadButton());
 	} else {
-		console.log(logId, "buttons not detected");
+		console.log(logId, "buttons not detected, download unavailable.");
 		document.body.style.border = "5px solid orange";
 	}
 }
@@ -48,6 +58,7 @@ const downloadRequestPort = browser.runtime.connect("", {
 	name: "franceculture-download-request",
 });
 
-onUrlChangedPort.onMessage.addListener(function (m) {
+// Évènement qui permet d'être notifié des navigations sur les onglets de franceculture.fr.
+onUrlChangedPort.onMessage.addListener(function () {
 	addDownloadButton();
 });

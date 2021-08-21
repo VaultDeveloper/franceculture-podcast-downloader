b72e5028980d1893b7a3f7219d5609c8658b955c
Marco <1583606+VaultDeveloper@users.noreply.github.com>
2021-08-21 10:10:02 +0200

    File location detection.

--- france-culture-downloader.js
+++ france-culture-downloader.js
@@ -5,26 +5,35 @@ console.log(logId, "Plugin loaded.");
 
 function addDownloadButton() {
 	const domButtons = document.querySelector("div.buttons");
-	
+
 	if (domButtons) {
 		console.log(logId, "buttons detected");
 		document.body.style.border = "5px solid green";
-	
+
 		const downloadButton = document.createElement("button");
 		downloadButton.className =
 			"replay-button paused aod playable blue textualized playing";
 		downloadButton.style = "font-size: inherit;";
 		downloadButton.innerHTML = "Télécharger";
 		domButtons.appendChild(downloadButton);
-	
+
 		downloadButton.addEventListener("click", () => {
 			// Todo lancer un téléchargement.
-			console.log(logId, "Asking download.");
+			console.log(logId, "Download button clicked.");
+			getFileUrl(domButtons);
 		});
 	} else {
 		console.log(logId, "buttons not detected");
 		document.body.style.border = "5px solid orange";
 	}
+}
+
+/** Retourne l'adresse du fichier pour le div qui contient tous les boutons. */
+function getFileUrl(buttons) {
+	const fileUrl = buttons.firstChild.getAttribute("data-url");
+	// Ne fonctionne que si c'est le premier button et que l'attribut pour stocker l'url du fichier est `data-url`.
+	console.log(`${logId} File location: ${fileUrl}.`);
+	return fileUrl;
 }
 
 addDownloadButton();

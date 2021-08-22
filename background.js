734e7b0b480dd0583ef88c73440790ae71f92384
Marco <1583606+VaultDeveloper@users.noreply.github.com>
2021-08-22 09:57:46 +0200

    Chaînage des téléchargements.

--- background.js
+++ background.js
@@ -23,6 +23,16 @@ let onNavigationChangedPort;
 /** Port pour communiquer les demandes de téléchargement. */
 let onDownloadRequestPort;
 
+/** Valeur du téléchargement en cours. Si aucun téléchargement `undefined`.
+ * @type {number}
+ */
+let currentDownloadId;
+
+/** Prochains fichiers à télécharger.
+ * @type {Array<DownloadRequestData>}
+ */
+const nextDownloads = [];
+
 /** Callback qui disperse les écoute sur les différents ports en fonction de l'attribut `name` passé en paramètre du port.
  * @param p Port à écouter.
  */
@@ -33,7 +43,7 @@ function onPortConnected(p) {
 		onNavigationChangedPort = p;
 	else if (p.name === "franceculture-download-request") {
 		onDownloadRequestPort = p;
-		onDownloadRequestPort.onMessage.addListener(downloadFile);
+		onDownloadRequestPort.onMessage.addListener(onDownloadRequestReceived);
 	} else console.error(`${logId} Port name not recognized '${p.name}'.`);
 }
 
@@ -42,13 +52,17 @@ function onPortConnected(p) {
  */
 function downloadFile(data) {
 	console.log(`${logId} Download request received.`);
+
 	const downloading = browser.downloads.download({
 		url: data.url,
 		filename: data.filename,
 	});
 	downloading.then(
-		() => {
-			console.log(`${logId} Download started.`);
+		(downloadId) => {
+			console.log(
+				`${logId} Download started (id: ${downloadId}, url: ${data.url}).`
+			);
+			currentDownloadId = downloadId;
 		},
 		(error) => {
 			console.error(`${logId} Download failed: ${error}`);
@@ -56,8 +70,73 @@ function downloadFile(data) {
 	);
 }
 
+/** Fonction appelée lorsqu'un téléchargement évolue.
+ * @param {DownloadDelta} delta Objet `DownloadDelta` (WebAPI) qui indique l'évolution du téléchargement.
+ */
+function onDownloadChanged(delta) {
+	console.log(`${logId} download changed:`);
+	console.log(delta);
+
+	if (
+		currentDownloadId === delta.id &&
+		(hasUserCanceled(delta) || hasDownloadJustFinished(delta))
+	) {
+		// Le téléchargement qui vient de terminer est celui de ce script.
+		onCurrentDownloadCompleted();
+	}
+}
+
+/** Retourne `true` si l'utilisateur a arrêté le téléchargement.
+ * @param {DownloadDelta} delta Objet `DownloadDelta` (WebAPI) qui indique l'évolution du téléchargement.
+ * @returns {boolean}
+ */
+function hasUserCanceled(delta) {
+	return delta.error && delta.error.current === "USER_CANCELED";
+}
+
+/** Retourne `true` si le téléchargement vient de se terminer.
+ * @param {DownloadDelta} delta Objet `DownloadDelta` (WebAPI) qui indique l'évolution du téléchargement.
+ * @returns {boolean}
+ */
+function hasDownloadJustFinished(delta) {
+	return (
+		delta.state &&
+		delta.state.current === "complete" &&
+		delta.state.previous === "in_progress"
+	);
+}
+
+/** Fonction appelée lorsque le téléchargement courant est terminé. */
+function onCurrentDownloadCompleted() {
+	currentDownloadId = undefined;
+	startDownloadIfReady();
+}
+
+/** Fonction appelée lorsqu'une nouvelle demande de téléchargement est reçue.
+ * @param {DownloadRequestData} data
+ */
+function onDownloadRequestReceived(data) {
+	nextDownloads.push(data);
+	startDownloadIfReady();
+}
+
+/** Démarre un téléchargement si aucun n'est en cours. */
+function startDownloadIfReady() {
+	console.log(`${logId} Checking if next download is ready.`);
+	if (currentDownloadId === undefined && nextDownloads.length > 0) {
+		// Si aucun élément n'est en cours de téléchargement et s'il reste des éléments à télécharger, on prend le premier (file).
+		console.log(`${logId} Next download ready.`);
+		downloadFile(nextDownloads.shift());
+	} else {
+		console.log(
+			`${logId} Nothing to download (current: ${currentDownloadId}, next: ${nextDownloads.length}).`
+		);
+	}
+}
+
 // Ajoute les écoutes sur des événements.
 browser.runtime.onConnect.addListener(onPortConnected);
+browser.downloads.onChanged.addListener(onDownloadChanged);
 
 /** Classe passé dans le port `onDownloadRequestPort` contenant les informations sur le fichier à télécharger. */
 class DownloadRequestData {
